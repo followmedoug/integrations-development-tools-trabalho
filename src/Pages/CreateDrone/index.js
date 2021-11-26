b4420c06fc44d66de5f8f4e416a13601eabc46b0
followmedoug <douglaspmateus@gmail.com>
2021-11-25 22:07:04 -0300

    tela de criaçao finalizada

--- src/Pages/CreateDrone/index.js
+++ src/Pages/CreateDrone/index.js
@@ -1,9 +1,45 @@
-import React from 'react'
-import ContainerMain from '../../Components/ContainerMain'
-import Title from '../../Components/Title'
+import React, { useState } from "react"
+import ContainerMain from "../../Components/ContainerMain"
+import Title from "../../Components/Title"
+import Form from "../../Components/Form"
+import Input from "../../Components/Input"
+import Toggle from "../../Components/Toggle"
+import WrapperFlex from "../../Components/WrapperFlex"
+import Button from "../../Components/Button"
 
 export default function CreateDrone() {
-    return <ContainerMain>
-        <Title>Criar Drone</Title>
+  const [name, setName] = useState("")
+  const [rastreavel, setRastreavel] = useState(false)
+
+  const handleCreate = () => {
+    console.log("vamo ver: ", name, rastreavel)
+  }
+
+  return (
+    <ContainerMain>
+      <Title>Criar Drone</Title>
+      <Form onSubmit={() => console.log("")}>
+        <Input
+          placeholder="Insira o nome do drone"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <WrapperFlex style={{ alignItems: "center" }}>
+          <span style={{ color: "#fff", marginRight: "10px" }}>
+            Drone será rastreável?
+          </span>
+          <Toggle handleCheck={() => setRastreavel(!rastreavel)} />
+        </WrapperFlex>
+      </Form>
+      <WrapperFlex
+        style={{ width: "100%", justifyContent: "center", margin: "15px 0" }}
+      >
+        <Button
+          style={{ width: "100%", maxWidth: "302px" }}
+          onClick={() => handleCreate()}
+        >
+          Salvar
+        </Button>
+      </WrapperFlex>
     </ContainerMain>
+  )
 }
--- src/Store/Sagas/drone.js
+++ src/Store/Sagas/drone.js
@@ -1,16 +1,30 @@
-import { call, put } from 'redux-saga/effects'
-import api from '../../Services/api'
+import { call, put } from "redux-saga/effects"
+import api from "../../Services/api"
 
-import { Creators as DroneActions } from '../Ducks/drone'
+import { Creators as DroneActions } from "../Ducks/drone"
 
 export function* getDrone() {
-    try {
-        const response = yield call(api.get, `/drone`)
+  try {
+    const response = yield call(api.get, `/drone`)
 
-        if(response.status === 200) {
-            yield put(DroneActions.getDronesSuccess(response.data))
-        }
-    } catch (error) {
-        throw new Error('houve um erro ao buscar drones')
+    if (response.status === 200) {
+      yield put(DroneActions.getDronesSuccess(response.data))
     }
+  } catch (error) {
+    throw new Error("houve um erro ao buscar drones")
+  }
+}
+
+export function* postDrone(action) {
+  try {
+    const { payload } = action
+
+    const response = yield call(api.post, "/drone", payload)
+
+    if (response.status === 200) {
+      yield put(DroneActions.postDroneSuccess())
+    }
+  } catch (error) {
+    throw new Error("Houve um erro ao cadastrar o drone")
+  }
 }
